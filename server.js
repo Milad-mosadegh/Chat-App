a2137fbd27a866c3edffb0aad84f5cd75b57c310
sdf <dfvsdfvadv>
2021-01-20 14:40:51 +0100

    sm

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static('client/build'));
 }
