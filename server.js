598fc60bb11dfed70062aea05d5843e67e015a17
sdf <dfvsdfvadv>
2021-01-20 14:22:55 +0100

    Add smth to Server.js

--- server.js
+++ server.js
@@ -25,8 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build'));
+}
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -45,10 +47,11 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
-}
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
